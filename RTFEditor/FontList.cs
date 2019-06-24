6c57a37872e39bc93e4c2cb9e3c2ffcb0d92d169
LinLu <wenhelinlu@gmail.com>
2019-06-24 14:32:08 +0800

    优化代码

--- RTFEditor/FontList.cs
+++ RTFEditor/FontList.cs
@@ -1,21 +1,28 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Drawing.Text;
 using System.Windows.Media;
-
 
 namespace RTFEditor
 {
     /// <summary>
     /// Erzeugt eine String Liste aller auf dem System installierten Schriftarten
     /// </summary>
-    class FontList : ObservableCollection<string> 
-    { 
-        public FontList() 
+    internal class FontList : ObservableCollection<string>
+    {
+        public FontList()
         {
-            foreach (FontFamily f in Fonts.SystemFontFamilies)
-            {                
-                this.Add(f.ToString());                
-            }  
-        }   
+            //foreach (FontFamily f in Fonts.SystemFontFamilies)
+            //{
+            //    this.Add(f.ToString());
+            //}
+            using (InstalledFontCollection fonts = new InstalledFontCollection())
+            {
+                foreach (var family in fonts.Families)
+                {
+                    this.Add(family.Name);
+                }
+            }
+        }
     }
 }
